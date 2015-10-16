246920204c7def463b65671b8760f7364b6650d7
Evan Sagge <evansagge@gmail.com>
2015-10-16 14:28:45 -0700

    Codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'simplecov'
-SimpleCov.start
+require "codeclimate-test-reporter"
+
+SimpleCov.start do
+  formatter CodeClimate::TestReporter::Formatter
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sidekiq/custom/queue'
